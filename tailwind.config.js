3e79975b458391dbaa5eb2b16cb9ebcf77de2a67
Yemima Sutanto <64170347+pootreth@users.noreply.github.com>
2023-07-25 19:28:46 +0700

    hero update

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
   content: ["index.html"],
   theme: {
-    extend: {}
+    container: {
+      center: true,
+      padding: '16px'
+    },
+    extend: {
+      colors: {
+        primary: '#1d4ed8',
+        dark: '#0f172a'
+      },
+      screens: {
+        '2xl' : '1320px',
+      }
+    }
   },
   plugins: []
 };
+
+// #92400e
+// #1d4ed8
